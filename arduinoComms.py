c9cb6b9da1b3cd9f40127a1019b30e5d2fa592ad
Rob Berwick <rob.berwick@gmail.com>
2014-09-06 17:53:24 +0100

    tidied and made more pythonic

--- arduinoComms.py
+++ arduinoComms.py
@@ -3,102 +3,87 @@ from time import sleep
 import audio
 from datetime import datetime
 
-def isNumber(c):
-    # Simple test for whether character is a number
-    if (c=='0' or c=='1' or c=='2' or c=='3' or c=='4' or c=='5' or c=='6' or c=='7' or c=='8' or c=='9'):
-        return True
-    return False
-
-def repeatSend(serial, commandChar, nSeconds, nInterval):
+def repeatSend(serial, command_verb, nSeconds, INTERVAL):
     # Send a single char repeatedly at a specific
     # interval for a specific length of time.
     # Have to play fireing sound here as it sounds as it lights LED
-    if (commandChar == 'P'):
+    if (command_verb == 'P'):
         # Play a sound to show that we are scanning
         audio.playSound(11)
 
-    if (commandChar == 'Z'):
+    if (command_verb == 'Z'):
+        LASER_REPEATS = 5
         # Uppercase 'Z' to turn LED ON
-        print(commandChar)
-        print(commandChar)
-        print(commandChar)
-        print(commandChar)
-        print(commandChar)
-        serial.write('Z')
-        serial.write('Z')
-        serial.write('Z')
-        serial.write('Z')
-        serial.write('Z')
+        print(command_verb * LASER_REPEATS)
+        serial.write('Z' * LASER_REPEATS)
         # Play sound
         audio.playSound(11)
-        # Lowert case 'z' to turn LED OFF
-        serial.write('z')
-        serial.write('z')
-        serial.write('z')
-        serial.write('z')
-        serial.write('z')
-        print('z')
-        print('z')
-        print('z')
-        print('z')
-        print('z')
+        # Lower case 'z' to turn LED OFF
+        serial.write('z' * LASER_REPEATS)
+        print('z' * LASER_REPEATS)
         sleep(0.3)
     else:
         while (nSeconds > 0.0):
-            print(commandChar)
-            serial.write(commandChar)
-            sleep(nInterval)
-            nSeconds = nSeconds - nInterval
+            print(command_verb)
+            serial.write(command_verb)
+            sleep(INTERVAL)
+            nSeconds = nSeconds - INTERVAL
 
-def sendCommandString(serial, commandChar, count):
+def send_command_string(serial, command_verb, command_value):
+    
+    # uppercase the command_verb
+    command_verb = command_verb.upper()
+
     # Simply write to the serial device
-    nInterval = 0.5 # In Seconds
-    nFBSeconds = 1.0
-    nLRSeconds = 0.125
-    nPSeconds = 0.5
+    INTERVAL = 0.5 # In Seconds
+    FB_SECONDS = 1.0
+    LR_SECONDS = 0.125
+    P_SECONDS = 0.5
     loop = 0
-    while (loop < count):
-        #print(str(loop))
-        if (commandChar == 'F' or commandChar == 'B'):
-            repeatSend(serial, commandChar, nFBSeconds, nInterval)
-        if (commandChar == 'L' or commandChar == 'R'):
-            repeatSend(serial, commandChar, nLRSeconds, nInterval)
-        if (commandChar == 'P' or commandChar == 'Z'):
-            repeatSend(serial, commandChar, nPSeconds, nInterval)
+    while (loop < command_value):
+        if (command_verb.upper() in ['F','B']:
+            repeatSend(serial, command_verb, FB_SECONDS, INTERVAL)
+        if (command_verb.upper() in['L', 'R']):
+            repeatSend(serial, command_verb, LR_SECONDS, INTERVAL)
+        if (command_verb.upper() in ['P', 'Z']):
+            repeatSend(serial, command_verb, P_SECONDS, INTERVAL)
         loop = loop + 1
 
-def parseCommandString(commandString):
+def parseCommandString(command_string):
+
     # Open the serial communication port
     ser = serial.Serial('/dev/ttyACM0', 9600)
 
     nPause = 0.5
     nIndex = 0
-    commandChar = ' '
-    commandCount = 0
+    command_verb = ' '
+    command_value = 0
     countString = '' # Initialise count string
-    for c in commandString:
-        if ( isNumber(c) == False ):
-            if (commandChar != ' '):
-                #print(" ")
+    chunk_length = 3 # our commands are always 3 char where [0] is verb, [1][2] is the value (left padded)
+
+    command_list = [command_string[i:i+step] for i in range(0, len(command_string), chunk_length)]
+
+    for c in command_string:
+        if not c.isdigit():
+            if command_verb != ' ':
                 sleep(nPause)
-                commandCount = int(countString)
-                sendCommandString(ser, commandChar, commandCount)
-            # 'c' should be the command character (but not garanteed, its only NOT A NUMBER)
-            commandChar = c
+                command_value = int(countString)
+                send_command_string(ser, command_verb, command_value)
+            # 'c' should be the command verb (but not guaranteed, its only NOT A NUMBER)
+            command_verb = c
 
             # New command char found, reset count info
             countString = ''
-            commandCount = 0
+            command_value = 0
         else:
             # Add int char to int string
             countString += c
 
         # If its the final char, send it
-        if (nIndex == len(commandString)-1 and commandChar != ' '):
-            #print(" ")
+        if nIndex == len(commandString)-1 and command_verb != ' ':
             sleep(nPause)
-            commandCount = int(countString)
-            sendCommandString(ser, commandChar, commandCount)
+            command_value = int(countString)
+            send_command_string(ser, command_verb, command_value)
 
         # Increment loop counter
         nIndex = nIndex+1
